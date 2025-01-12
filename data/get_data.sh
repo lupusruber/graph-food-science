1f3267fd7b574aac608b9c72f1bdd4f5ab13dc04
lupusruber <petarpro234@gmail.com>
2025-01-12 18:21:09 +0100

    models + results

---
+++ data/get_data.sh
@@ -0,0 +1,2 @@
+cd data
+wget https://raw.githubusercontent.com/food-hazard-detection-semeval-2025/food-hazard-detection-semeval-2025.github.io/refs/heads/main/data/incidents_train.csv
--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from graph-food-science!")
-
-
-if __name__ == "__main__":
-    main()
